0099402589f7edcf50b108ebb663442c4551ca62
Andreas Fast <andreas.fast@moove-it.com>
2015-03-07 20:41:29 -0200

    Adjust c code to HalloWelt

--- ext/hallo_welt/hallo_welt.c
+++ ext/hallo_welt/hallo_welt.c
@@ -6,8 +6,8 @@ static VALUE hallo_welt_hello(VALUE self) {
 }
 
 void Init_hallo_welt(void) {
-  VALUE cHelloWorld;
+  VALUE cHalloWelt;
 
-  cHelloWorld = rb_const_get(rb_cObject, rb_intern("HalloWelt"));
-  rb_define_method(cHelloWorld, "hello", hallo_welt_hello, 0);
+  cHalloWelt = rb_const_get(rb_cObject, rb_intern("HalloWelt"));
+  rb_define_method(cHalloWelt, "hello", hallo_welt_hello, 0);
 }
